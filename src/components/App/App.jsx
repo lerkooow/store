94b849ab5e3666e13d8582c36d3d541f11a313b8
lerkooow <lerkoooow@gmail.com>
2024-01-23 18:39:38 +0500

    products component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,12 +7,14 @@ import Footer from "../Footer/Footer";
 import Sidebar from "../Sidebar/Sidebar";
 
 import { getCategories } from "../../features/categories/categoriesSlice";
+import { getProducts } from "../../features/products/productsSlice";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCategories());
+    dispatch(getProducts());
   }, [dispatch]);
 
   return (
