1df04b317b1dfe2e4c94950908b6379b3ef359c5
lerkooow <lerkoooow@gmail.com>
2024-01-25 16:23:43 +0500

    creating a product page

---
+++ src/components/Products/Product.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { ROUTES } from "../../utils/routes";
+
+import styles from "../../styles/Product.module.css";
+
+const SIZES = [4, 4.5, 5];
+
+const Product = ({ price, title, images, description }) => {
+  const [currentImage, setCurrentImage] = useState();
+  const [currentSize, setCurrentSize] = useState();
+
+  useEffect(() => {
+    if (!images.length) return;
+    setCurrentImage(images[0]);
+  }, [images]);
+
+  return (
+    <section className={styles.product}>
+      <div className={styles.images}>
+        <div
+          className={styles.current}
+          style={{ backgroundImage: `url(${currentImage})` }}
+        />
+        <div className={styles["images-list"]}>
+          {images.map((image, i) => (
+            <div
+              key={i}
+              className={styles.image}
+              style={{ backgroundImage: `url(${image})` }}
+              onClick={() => setCurrentImage(image)}
+            />
+          ))}
+        </div>
+      </div>
+      <div className={styles.info}>
+        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.price}>{price}$</div>
+        <div className={styles.color}>
+          <span>Color:</span> Green
+        </div>
+        <div className={styles.sizes}>
+          <span>Sizes:</span>
+          <div className={styles.list}>
+            {SIZES.map((size) => (
+              <div
+                className={`${styles.size} ${
+                  currentSize === size ? styles.active : ""
+                }`}
+                onClick={() => setCurrentSize(size)}
+                key={size}
+              >
+                {size}
+              </div>
+            ))}
+          </div>
+        </div>
+        <p className={styles.description}>{description}</p>
+        <div className={styles.actions}>
+          <button className={styles.add} disabled={!currentSize}>
+            Add to cart
+          </button>
+          <button className={styles.favourite}>Add to favourites</button>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.purchase}>19 people purchase</div>
+          <Link to={ROUTES.HOME}>Return to store</Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Home from "../Home/Home";
+
+import { ROUTES } from "../../utils/routes";
+import SingleProduct from "../Products/SingleProduct";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
       {/* index указывает на индексный (главный) маршрут */}
+      <Route path={ROUTES.PRODUCT} element={<SingleProduct />}></Route>
     </Routes>
   );
 };
