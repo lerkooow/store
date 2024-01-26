5a51b3d9d7a69125d83f93355ffb4d050ccbece9
lerkooow <lerkoooow@gmail.com>
2024-01-26 18:30:45 +0500

    category specific component

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../Home/Home";
-import SingleProduct from "../Products/SingleProduct";
 import Profile from "../Profile/Profile";
+import SingleProduct from "../Products/SingleProduct";
+import SingleCategory from "../Categories/SingleCategory";
 
 import { ROUTES } from "../../utils/routes";
 
@@ -14,6 +15,7 @@ const AppRoutes = () => {
       {/* index указывает на индексный (главный) маршрут */}
       <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
       <Route path={ROUTES.PROFILE} element={<Profile />} />
+      <Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
     </Routes>
   );
 };
